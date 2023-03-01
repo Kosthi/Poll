8380e0e1d0277b9ecd73d3199d02d38b3fd219dd
Koschei <nitianzero@gmail.com>
2023-03-01 12:40:52 +0800

    feat: create form

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_questions_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,9 +18,22 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = 'You\'re looking at the result of question %s'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse('You\'re voting on question %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.filter(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did\'t select a choice',
+        })
+    else:
+        # select_choice.votes += 1
+        # select_choice.save()
+        # 避免竞争条件
+        select_choice.update(votes=F('votes') + 1)
+        return HttpResponseRedirect(reverse('polls:result', args=(question_id,)))
 
