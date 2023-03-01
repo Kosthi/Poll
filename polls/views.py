bf27e372bc0fb54af21666c19e617d2b0c45a3b2
Koschei <nitianzero@gmail.com>
2023-03-01 13:02:57 +0800

    refactor: using generic views

--- polls/views.py
+++ polls/views.py
@@ -3,23 +3,25 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_questions_list': latest_questions_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """返回最后5个发布的问题"""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
